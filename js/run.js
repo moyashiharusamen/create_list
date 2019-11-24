a9ab0f42eab94b06b80ac9d28d98d12596440cc8
冨永将司 <moyashi.harusamen@gmail.com>
2019-11-24 22:15:54 +0900

    関数ではなく、fetch 内に直接記述

--- js/run.js
+++ js/run.js
@@ -2,70 +2,66 @@
     const jsonPath = '../data/list.json';
 
     fetch(jsonPath)
-        .then((responce) => responce.json())
-        .then((json) => {
-            createContents(json);
-        });
+        .then(responce => responce.json())
+        .then(json => {
+            const l = json.length;
+            const headText = ['No.', '名称', '提出日', '備考'];
+            const headTextLength = headText.length;
 
-    const createContents = (json) => {
-        const l = json.length;
-        const headText = ['No.', '名称', '提出日', '備考'];
-        const headTextLength = headText.length;
+            for (let i = 0; i < l; i++) {
+                const h2 = document.createElement('h2');
+                const table = document.createElement('table');
+                const thead = table.createTHead();
+                const tbody = table.createTBody();
+                const row = thead.insertRow(0);
 
-        for (let i = 0; i < l; i++) {
-            const h2 = document.createElement('h2');
-            const table = document.createElement('table');
-            const thead = table.createTHead();
-            const tbody = table.createTBody();
-            const row = thead.insertRow(0);
+                for (let k = 0; k < headTextLength; k++) {
+                    let cell = row.insertCell(k);
 
-            for (let k = 0; k < headTextLength; k++) {
-                let cell = row.insertCell(k);
+                    cell.innerText = headText[k];
+                }
 
-                cell.innerText = headText[k];
-            }
+                Object.keys(json[i]).forEach((e) => {
+                    const l2 = json[i][e].length;
 
-            Object.keys(json[i]).forEach((e) => {
-                const l2 = json[i][e].length;
+                    h2.innerText = e;
+                    list.appendChild(h2);
+                    list.insertBefore(table, h2.nextSibling);
 
-                h2.innerText = e;
-                list.appendChild(h2);
-                list.insertBefore(table, h2.nextSibling);
+                    for (let j = 0; j < l2; j++) {
+                        const tbodyRow = tbody.insertRow(0);
+                        const value = json[i][e][j];
+                        const content = {
+                            num: value.number,
+                            name: value.name,
+                            date: value.date,
+                            note: value.note,
+                        };
+                        const status = value.status;
+                        const path = value.path;
 
-                for (let j = 0; j < l2; j++) {
-                    const tbodyRow = tbody.insertRow(0);
-                    const value = json[i][e][j];
-                    const content = {
-                        num: value.number,
-                        name: value.name,
-                        date: value.date,
-                        note: value.note,
-                    };
-                    const status = value.status;
-                    const path = value.path;
+                        tbodyRow.classList = status;
 
-                    tbodyRow.classList = status;
+                        Object.keys(content).forEach((e, i) => {
+                            const a = document.createElement('a');
+                            let cell = tbodyRow.insertCell(i);
 
-                    Object.keys(content).forEach((e, i) => {
-                        const a = document.createElement('a');
-                        let cell = tbodyRow.insertCell(i);
+                            if (e === 'name') {
+                                cell.appendChild(a);
+                                cell.children[0].setAttribute('href', path);
+                                cell.children[0].innerText = content.name;
 
-                        if (e === 'name') {
-                            cell.appendChild(a);
-                            cell.children[0].setAttribute('href', path);
-                            cell.children[0].innerText = content.name;
+                                if (path.indexOf('http') !== -1) {
+                                    cell.children[0].setAttribute('target', '_blank');
+                                }
 
-                            if (path.indexOf('http') !== -1) {
-                                cell.children[0].setAttribute('target', '_blank');
+                                return;
                             }
 
-                            return;
-                        }
-
-                        cell.innerText = content[e];
-                    });
-                }
-            })
-        }
-    };
+                            cell.innerText = content[e];
+                        });
+                    }
+                })
+            }
+        });
 }
