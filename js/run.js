022e8691df22e6bae00e6596f361c136c81e9c96
冨永将司 <moyashi.harusamen@gmail.com>
2019-11-24 21:44:49 +0900

    機能の作成

--- js/run.js
+++ js/run.js
@@ -10,24 +10,8 @@
 
     const hoge = (json) => {
         const l = json.length;
-        // const table =
-        //     `<table class="list-table">
-        //     <thead>
-        //     <tr>
-        //     <th>No.</th>
-        //     <th>名称</th>
-        //     <th>提出日</th>
-        //     <th>備考</th>
-        //     </tr>
-        //     </thead>
-        //     <tbody>
-        //     </tbody>
-        //     </table>`;
         const headText = ['No.', '名称', '提出日', '備考'];
         const headTextLength = headText.length;
-        const a = document.createElement('a');
-
-        // console.log(table);
 
         for (let i = 0; i < l; i++) {
             const h2 = document.createElement('h2');
@@ -50,51 +34,39 @@
                 list.insertBefore(table, h2.nextSibling);
 
                 for (let j = 0; j < l2; j++) {
-                    const content = {
-                        num: json[i][e][j].number,
-                        name: json[i][e][j].name,
-                        note: json[i][e][j].note,
-                        status: json[i][e][j].status
-                    };
                     const tbodyRow = tbody.insertRow(0);
+                    const value = json[i][e][j];
+                    const content = {
+                        num: value.number,
+                        name: value.name,
+                        date: value.date,
+                        note: value.note,
+                    };
+                    const status = value.status;
+                    const path = value.path;
 
-                    tbodyRow.classList = content.status;
+                    tbodyRow.classList = status;
 
                     Object.keys(content).forEach((e, i) => {
+                        const a = document.createElement('a');
                         let cell = tbodyRow.insertCell(i);
 
-                        cell.innerText = content[e];
-
-                        if (i === 1) {
-                            console.log(cell);
+                        if (e === 'name') {
                             cell.appendChild(a);
+                            cell.children[0].setAttribute('href', path);
+                            cell.children[0].innerHTML = content.name;
+
+                            if (path.indexOf('http') !== -1) {
+                                cell.children[0].setAttribute('target', '_blank');
+                            }
+
+                            return;
                         }
+
+                        cell.innerText = content[e];
                     });
-
-                    console.log(table);
-
-                    // tr =
-                    //     `<tr class="${content.status}">
-                    //     <td>${content.number}</td>
-                    //     <td><a href="${content.path}">${content.name}</a></td>
-                    //     <td>${content.date}</td>
-                    //     <td>${content.note}</td>
-                    //     </tr>
-                    //     `;
                 }
             })
         }
-
-        Object.keys(json).forEach((e) => {
-            const length = json[e].length;
-            // const heading = `<h2>${e}</h2>`;
-            // let tr;
-
-            // list.insertAdjacentHTML('afterbegin', heading);
-
-            // for (let i = 0; i < length; i++) {
-            //     const content = json[e][i];
-            // }
-        });
     };
 }
